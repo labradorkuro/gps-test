fb266adf4b76dd4c3c58d71bcc5ffabf566bf9f9
Takenori Tanaka <jazzsaxplayer02@gmail.com>
2017-03-07 15:06:01 +0900

    first

---
+++ gps-test/models/location.js
@@ -0,0 +1,22 @@
+﻿//
+// 2016.01.27 t.tanaka
+// Model
+// 休日設定
+//
+var Sequelize = require('sequelize');
+
+var Location = sequelize.define('location_tbl', {
+  id: { type: Sequelize.INTEGER, autoIncrement: true ,primaryKey: true},
+	serialno: Sequelize.STRING,
+	latitude: Sequelize.STRING,
+	longitude: Sequelize.STRING,
+	altitude: Sequelize.STRING
+},
+{
+	schema:'location_system',
+	underscored: true,
+	timestamps: false
+});
+module.exports = function (sequelize, DataTypes) {
+	return Location;
+};
