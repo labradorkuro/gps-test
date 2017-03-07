3ca6fd8fc6edaf52807abf90413b1b2c9bd6f914
Takenori Tanaka <jazzsaxplayer02@gmail.com>
2017-03-07 17:39:39 +0900

    add post-test

--- gps-test/app.js
+++ gps-test/app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
+var post-test = require('./routes/post-test');
 var users = require('./routes/users');
 var config = require('config');
 
@@ -29,6 +30,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
+app.use('/post-test', post-test);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
