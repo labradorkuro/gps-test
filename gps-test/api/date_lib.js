747346bb5682ec5ceb3ae7a577151bfa17e1d56f
Takenori Tanaka <jazzsaxplayer02@gmail.com>
2017-04-04 13:50:16 +0900

    modify date_lib.js

--- gps-test/api/date_lib.js
+++ gps-test/api/date_lib.js
@@ -1,26 +1,26 @@
-var date_lib = date_lib || {}
+module.exports = {
 
 // 日付処理
-date_lib.getToday = function (format_str) {
+getToday : function (format_str) {
 	var d = new Date();
 	return date_lib.getDateString(d, format_str) + " " + date_lib.getTimeString(d,"{0}:{1}:00");
-};
-date_lib.getDateString = function (date, format_str) {
+},
+getDateString : function (date, format_str) {
 	var date_format = date_lib.format(format_str,
 			date.getFullYear(),
 			date.getMonth() + 1 < 10 ? "0" + (date.getMonth() + 1) : (date.getMonth() + 1),
 		    date.getDate() < 10 ? "0" + date.getDate() : date.getDate()
 	);
 	return date_format;
-};
-date_lib.getTimeString = function (date, format_str) {
+},
+getTimeString : function (date, format_str) {
 	var date_format = date_lib.format(format_str,
 			date.getHours() < 10 ? "0" + date.getHours() : date.getHours(),
 			date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes()
 	);
 	return date_format;
-};
-date_lib.format = function (fmt, a) {
+},
+format : function (fmt, a) {
 	var rep_fn = undefined;
 
 	if (typeof a == "object") {
@@ -32,4 +32,5 @@ date_lib.format = function (fmt, a) {
 	}
 
 	return fmt.replace(/\{(\w+)\}/g, rep_fn);
+}
 };
